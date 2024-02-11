8bce0263450b5ba293f97ecf5c4425ebf1dd50cf
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-02-11 16:52:12 +0800

    feat: add toast
    This commmit adds a toast to notify the user that the image is still generating

--- resources/js/Pages/Home.svelte
+++ resources/js/Pages/Home.svelte
@@ -1,10 +1,13 @@
 <script>
+    import toast, { Toaster } from "svelte-french-toast";
+
     let user = "itsmenewbie03";
     let img_src =
-        "https://replicate.delivery/pbxt/9x87a9i8adKdJZoiMbbnfdfCXX3e6VLIO53X5gx9XbM7beLJB/out-0.png";
+        "https://replicate.delivery/pbxt/ERGpwDTPEtIlDdgNKs3OkvkkpGe8fT1sPf4uVBpzagcUhHrkA/out-0.png";
     let desc = "";
 
     const gen_img = async () => {
+        const id = toast.loading("Generating Image...");
         // TODO: call internal api
         const resp = await fetch("api/image/gen", {
             method: "POST",
@@ -16,20 +19,24 @@
             }),
         }).then((res) => res.json());
         img_src = resp.img_url ?? img_src;
+        toast.dismiss(id);
     };
 </script>
 
+<Toaster />
 <div
     class="container min-h-screen min-w-full p-5 flex flex-col bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white"
 >
     <p class="text-3xl text-white text-center font-bold">
         Simple Text to Image Tool built with Laravel by {user}
     </p>
-    <img
-        class="rounded-lg mt-1 h-full max-h-[450px] object-scale-down aspect-square mb-2"
-        src={img_src}
-        alt="nothing xD"
-    />
+    <div class="w-full flex justify-center">
+        <img
+            class="rounded-lg mt-1 max-h-[450px] object-cover h-full aspect-square mb-2"
+            src={img_src}
+            alt="nothing xD"
+        />
+    </div>
     <input
         type="text"
         class="input input-bordered input-primary"
